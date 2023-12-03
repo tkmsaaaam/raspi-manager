601158e75a70812e0755a3783d707a2973fb0a85
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-03 21:26:37 +0900

    fix envname

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TEXT=`clamscan / -r -i`
-curl -H "Authorization: Bearer $TOKEN" -d "text=$TEXT" "https://slack.com/api/chat.postMessage?channel=$CHANNEL"
+curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
