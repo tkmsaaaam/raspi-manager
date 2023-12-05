e9b4b110e2744b6e72c2428ca7d1698700aae825
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-05 19:36:28 +0900

    fix shellscripts

--- scripts/checklogin.sh
+++ scripts/checklogin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd `dirname $-0`
+source ./env.sh
 TEXT='ログイン履歴\n'
 RESULT=`cat -n /var/log/auth.log | grep systemd-logind | tail -n 50`
 curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+exit 0
--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd `dirname $-0`
+source ./env.sh
 TEXT='スキャン結果\n'
 RESULT=`clamscan / -r -i`
 curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+exit 0
