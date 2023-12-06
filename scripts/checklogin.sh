4e9d31e24838ff6afd85f68ec27a1d568bca4eb6
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-06 22:55:34 +0900

    add options

--- scripts/checklogin.sh
+++ scripts/checklogin.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`
 source ./env.sh
 TEXT='ログイン履歴\n'
-RESULT=`cat -n /var/log/auth.log | grep systemd-logind | tail -n 50`
+RESULT=`cat -n /var/log/auth.log | grep -a -e systemd-logind -e sshd | tail -n 50`
 curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
 exit 0
