2cac5f198bcbeb5d46c1033049d0c93ddb88bd95
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-07 21:23:46 +0900

    fix shellscript

--- scripts/checklogin.sh
+++ scripts/checklogin.sh
@@ -2,6 +2,7 @@
 cd `dirname $0`
 source ./env.sh
 TEXT='ログイン履歴\n'
-RESULT=`cat -n /var/log/auth.log | grep -a -e systemd-logind -e sshd | tail -n 50`
+DATE=`date '+%b %e'`
+RESULT=`cat -n /var/log/auth.log | grep -a -e systemd-logind -e sshd | grep -a $DATE`
 curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
 exit 0
