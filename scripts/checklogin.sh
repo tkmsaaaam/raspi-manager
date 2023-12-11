60f097b49745005146a4173e564c39b870a17a8c
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-11 22:23:36 +0900

    remove unnecessary file

--- scripts/checklogin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-source ./env.sh
-TEXT='ログイン履歴\n'
-DATE=`date '+%b %e'`
-RESULT=`cat -n /var/log/auth.log | grep -a -e systemd-logind -e sshd | grep -a "$DATE"`
-curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
-exit 0
