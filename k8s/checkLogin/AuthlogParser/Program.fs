d30a019f68daf9d59ea354eabc191fffeb4c98aa
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-12-30 11:25:54 +0900

    update

---
+++ k8s/checkLogin/AuthlogParser/Program.fs
@@ -0,0 +1,18 @@
+﻿[<EntryPoint>]
+let main args =
+    let today = System.DateTime.Today
+    let filterMonth (month: string) =
+        month.Equals(today.ToString "MMM" )
+    let filterDate (date: string) =
+        date.Equals(string today.Day)
+    let filterDaemon (daemon: string) =
+        daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
+    let filterLine (line: string) =
+        line.Split()[0] |> filterMonth && line.Split()[1] |> filterDate && line.Split()[4] |> filterDaemon
+    let file = @"/logs/auth.log"
+    let lines = System.IO.File.ReadAllLines(file)
+    let filterLines (lines: string array) =
+        lines |> Array.filter (fun line -> filterLine line)
+    for line in filterLines lines do
+        printfn "%s" line
+    0
