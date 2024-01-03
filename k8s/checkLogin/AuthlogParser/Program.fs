50c1c38458e4a85694b40563c6bae8b16ddade97
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-03 10:28:40 +0900

    use formatted authlog

--- k8s/checkLogin/AuthlogParser/Program.fs
+++ k8s/checkLogin/AuthlogParser/Program.fs
@@ -1,15 +1,13 @@
 ﻿[<EntryPoint>]
 let main args =
     let today = System.DateTime.Today
-    let filterMonth (month: string) =
-        month.Equals(today.ToString "MMM" )
     let filterDate (date: string) =
-        date.Equals(string today.Day)
+        date.Equals(today.ToString "yyyy/MM/dd" )
     let filterDaemon (daemon: string) =
         daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
-    let filterLine (line: string) = // TODO: fix filterDate, filterDaemon
-        line.Split()[0] |> filterMonth && line.Split()[2] |> filterDate && line.Split()[5] |> filterDaemon
-    let file = @"/logs/auth.log"
+    let filterLine (line: string) =
+        line.Split("|")[0] |> filterDate && line.Split("|")[5] |> filterDaemon
+    let file = @"/logs/authlog"
     let lines = System.IO.File.ReadAllLines(file)
     let filterLines (lines: string array) =
         lines |> Array.filter (fun line -> filterLine line)
