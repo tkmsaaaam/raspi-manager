5d4a6822dd83971adff3eb21708efeb0b3fdd0e4
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-06 09:55:04 +0900

    fix failed cronjob

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,10 +3,11 @@ cd $(dirname $0)
 basedir=`pwd`
 find k8s | grep Dockerfile | while read filename
 do
-  pwd
   path=`echo $filename | sed "s#/Dockerfile##g"`
   cd $path
+  pwd
   imageName=`cat Dockerfile | head -n 1 | sed "s/# //g"`
+  echo $imageName
   docker build . -t $imageName $1
   cd $basedir
 done
