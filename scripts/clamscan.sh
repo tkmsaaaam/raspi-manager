6318da24d69799991b571bffa5969e4e88459b50
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-08 16:08:16 +0900

    use secretKeyRef

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -3,5 +3,5 @@ cd `dirname $0`
 source ./env.sh
 TEXT="スキャン結果"$'\n'
 RESULT=`clamscan / -r -i`
-curl -H "Authorization: Bearer $SLACK_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
 exit 0
