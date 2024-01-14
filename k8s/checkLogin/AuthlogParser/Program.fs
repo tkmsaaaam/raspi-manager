89b5ec9afae44663946c2ca211a22901f3f449a6
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-14 09:56:59 +0900

    update message && fix timezone

--- k8s/checkLogin/AuthlogParser/Program.fs
+++ k8s/checkLogin/AuthlogParser/Program.fs
@@ -1,16 +1,20 @@
 ﻿[<EntryPoint>]
 let main args =
-    let today = System.DateTime.Today
+    let jstTimeZone =
+      System.TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+    let today =
+      System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone);
     let filterDate (date: string) =
-        date.Equals(today.ToString "yyyy/MM/dd" )
+      date.Equals(today.ToString "yyyy/MM/dd" )
     let filterDaemon (daemon: string) =
-        daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
+      daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
     let filterLine (line: string) =
-        line.Split("|")[0] |> filterDate && line.Split("|")[5] |> filterDaemon
+      line.Split("|")[0] |> filterDate && line.Split("|")[5] |> filterDaemon
     let file = @"/logs/authlog"
     let lines = System.IO.File.ReadAllLines(file)
     let filterLines (lines: string array) =
-        lines |> Array.filter (fun line -> filterLine line)
+      lines |> Array.filter (fun line -> filterLine line)
+    let createMessage (line: string) = line[11..]
     for line in filterLines lines do
-        printfn "%s" line
+      createMessage line |> printfn "%s"
     0
