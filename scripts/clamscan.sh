88ed4224ba181f16fed89d37f61c3e0347039e75
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-22 07:51:32 +0900

    update

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 source ./env.sh
 TEXT="スキャン結果"$'\n'
-RESULT=`clamscan / -r -i`
+RESULT=`clamdscan -i $1`
 echo "$RESULT" > ./clamscan.log
 curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
 exit 0
