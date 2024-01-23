72e20d22cad5b81e3d3cc2c50b17df1be995302d
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-01-23 20:09:33 +0900

    remove unnecessary log file

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -3,6 +3,5 @@ cd `dirname $0`
 source ./env.sh
 TEXT="スキャン結果"$'\n'
 RESULT=`clamdscan -i $1`
-echo "$RESULT" > ./clamscan.log
 curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
 exit 0
