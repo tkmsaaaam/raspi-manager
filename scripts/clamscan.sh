330e2eac2e02edb79949ba99edf0de086cf1f6de
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-02-01 21:31:19 +0900

    fix

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -3,5 +3,6 @@ cd `dirname $0`
 source ./env.sh
 TEXT="スキャン結果"$'\n'
 RESULT=`clamdscan -i $1`
-curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+echo "$RESULT" >> /var/log/clamdscan.log
+curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL" > /var/log/scannotify.log 2>>&1
 exit 0
