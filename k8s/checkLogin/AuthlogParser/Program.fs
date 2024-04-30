66e13eb2d41387f48c134e2025a3b0292cffb9e3
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-05-01 08:29:17 +0900

    refactor

--- k8s/checkLogin/AuthlogParser/Program.fs
+++ k8s/checkLogin/AuthlogParser/Program.fs
@@ -1,23 +1,32 @@
 ﻿[<EntryPoint>]
 let main args =
-    let jstTimeZone =
-      System.TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
-    let today =
-      System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone).ToString("MMM d", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
-    let timeLength = System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone).ToString("MMM d HH:mm:ss", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")).Length
-    let hostname= System.Environment.GetEnvironmentVariable("HOSTNAME");
-    let hostnameLength =  hostname.Length
-    let filterDate (date: string) = date.StartsWith(today)
+    let jstTimeZone = System.TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time")
+    let format = System.Globalization.CultureInfo.CreateSpecificCulture("en-US")
+    let today = System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone)
+    let todayMmmD = today.ToString("MMM d", format)
+    let timeLength = today.ToString("MMM d HH:mm:ss", format).Length
+    let hostname = System.Environment.GetEnvironmentVariable("HOSTNAME")
+    let hostnameLength = hostname.Length
+    let filterDate (date: string) = date.StartsWith(todayMmmD)
+
     let filterDaemon (daemon: string) =
-      daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
+        daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
+
     let filterLine (line: string) =
-      line |> filterDate && line[timeLength+1+hostnameLength+1..] |> filterDaemon
+        line |> filterDate
+        && line[timeLength + 1 + hostnameLength + 1 ..] |> filterDaemon
+
     let file = @"/logs/auth.log"
     let lines = System.IO.File.ReadAllLines(file)
-    let filterLines (lines: string array) = lines |> Array.filter (fun line -> filterLine line)
+
+    let filterLines (lines: string array) =
+        lines |> Array.filter (fun line -> filterLine line)
+
     printfn "searching..."
-    today |> printfn "date: %s"
+    todayMmmD |> printfn "date: %s"
     hostname |> printfn "host: %s"
+
     for line in filterLines lines do
-      line |> printfn "%s"
+        line |> printfn "%s"
+
     0
