b1ae6a92848c456b22b02d35fbfd0b19d2963456
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-04-30 13:30:32 +0900

    update filter

--- k8s/checkLogin/AuthlogParser/Program.fs
+++ k8s/checkLogin/AuthlogParser/Program.fs
@@ -3,20 +3,21 @@ let main args =
     let jstTimeZone =
       System.TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
     let today =
-      System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone);
-    let timeLength =
-      today.ToString("MMM d HH:mm:ss", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")).Length
-    let hostnameLength =  System.Environment.GetEnvironmentVariable("HOSTNAME").Length
-    let filterDate (date: string) =
-      date.StartsWith(today.ToString("MMM d", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")))
+      System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone).ToString("MMM d", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+    let timeLength = System.TimeZoneInfo.ConvertTime(System.DateTime.Today, jstTimeZone).ToString("MMM d HH:mm:ss", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")).Length
+    let hostname= System.Environment.GetEnvironmentVariable("HOSTNAME");
+    let hostnameLength =  hostname.Length
+    let filterDate (date: string) = date.StartsWith(today)
     let filterDaemon (daemon: string) =
       daemon.StartsWith("sshd") || daemon.StartsWith("systemd-logind")
     let filterLine (line: string) =
       line |> filterDate && line[timeLength+1+hostnameLength+1..] |> filterDaemon
     let file = @"/logs/auth.log"
     let lines = System.IO.File.ReadAllLines(file)
-    let filterLines (lines: string array) =
-      lines |> Array.filter (fun line -> filterLine line)
+    let filterLines (lines: string array) = lines |> Array.filter (fun line -> filterLine line)
+    printfn "searching..."
+    today |> printfn "date: %s"
+    hostname |> printfn "host: %s"
     for line in filterLines lines do
       line |> printfn "%s"
     0
