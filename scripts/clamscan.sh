e5f6a433757f2597130fc54ddb358300d4568e6b
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-06-06 20:10:23 +0900

    use slackPublisher

--- scripts/clamscan.sh
+++ scripts/clamscan.sh
@@ -4,6 +4,7 @@ source ./env.sh
 export TEXT="スキャン結果"$'\n'
 export RESULT=`clamdscan -i $1`
 echo "$RESULT" >> /var/log/clamdscan.log
-export MESSAGE=`echo "$RESULT" | grep -v "Access denied. ERROR"`
-curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$MESSAGE" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+export BODY=`echo "$RESULT" | grep -v "Access denied. ERROR"`
+export MESSAGE=$TEXT$BODY
+./slackPublisher
 exit 0
--- scripts/kubestatus.sh
+++ scripts/kubestatus.sh
@@ -3,5 +3,6 @@ cd `dirname $0`
 source ./env.sh
 export TEXT="pods"$'\n'
 export RESULT=`kubectl get pods -A | grep -v "Running"`
-curl -H "Authorization: Bearer $SLACK_BOT_TOKEN" -d "text=$TEXT$RESULT" "https://slack.com/api/chat.postMessage?channel=$SLACK_CHANNEL"
+export MESSAGE=$TEXT$RESULT
+./slackPublisher
 exit 0
