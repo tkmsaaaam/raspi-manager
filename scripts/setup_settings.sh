56bfe647d06092066d4498f84687ed4f2435cc2f
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-04-13 16:09:55 +0900

    feat: add Nginx conf && setup script

---
+++ scripts/setup_settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(dirname $0)
+cd ..
+$rootdir=`pwd`
+
+# Set up Nginx configuration
+sudo rm -rf /etc/nginx/conf.d
+sudo ln -s $rootdir/symlinks/etc/nginx/conf.d/ /etc/nginx/conf.d
+sudo systemctl restart nginx
