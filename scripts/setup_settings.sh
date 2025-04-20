337210c9b01f4d1886dcb5c58ac967d41d0716b7
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-04-20 18:45:25 +0900

    feat: update setup_settings.sh

--- scripts/setup_settings.sh
+++ scripts/setup_settings.sh
@@ -4,8 +4,14 @@ cd $(dirname $0)
 cd ..
 rootdir=`pwd`
 
+# Set up promtail configuration
+sudo ln -s $rootdir/symlinks/etc/ptomtail/config.yml /etc/promtail/config.yml
+
+# Set up prometheus configuration
+sudo mkdir /etc/prometheus
+sudo cp $rootdir/settings/etc/prometheus/prometheus.yml /etc/prometheus/prometheus.yml
+
 # Set up Nginx configuration
 sudo rm -rf /etc/nginx/conf.d
-sudo ln -s $rootdir/symlinks/etc/ptomtail/config.yml /etc/promtail/config.yml
 sudo ln -s $rootdir/symlinks/etc/nginx/conf.d/ /etc/nginx/conf.d
 sudo systemctl restart nginx
