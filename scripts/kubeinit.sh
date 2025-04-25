8963d4cdbf3f642efb81b2721bd3dfa9c81c01df
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-04-25 10:10:32 +0900

    feat: install metrics-server with url instad of copied yaml

--- scripts/kubeinit.sh
+++ scripts/kubeinit.sh
@@ -6,6 +6,10 @@ kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Doc
 # kubectl taint nodes --all node-role.kubernetes.io/control-plane-node/k8s-cplane.novalocal untainted
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 kubectl create namespace tasks
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -k https://github.com/google/cadvisor//deploy/kubernetes/base
+
 # https://kubernetes.io/ja/docs/setup/production-environment/tools/kubeadm/
 # https://tech.virtualtech.jp/entry/2022/08/24/172753
 # https://github.com/cri-o/packaging/blob/main/README.md#usage
