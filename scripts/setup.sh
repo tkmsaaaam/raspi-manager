f628347c51feb0d6b35b1ae73294d87482848fe4
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-04-30 21:10:11 +0900

    feat: add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install ufw nginx avahi-daemon
+
+# https://kubernetes.io/ja/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#kubeadm-kubelet-kubectl%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB
